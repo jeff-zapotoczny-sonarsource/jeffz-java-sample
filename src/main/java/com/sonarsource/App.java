dc8bb21ea821d681bdd4c98940a375e652172e17
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2019-04-01 16:27:31 -0500

    Adding a stupid loop.

--- src/main/java/com/sonarsource/App.java
+++ src/main/java/com/sonarsource/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        doStuff();
+    }
+    
+    public static void doStuff() {
+    	for (int i = 0; i < 100; i++) {
+    		System.out.println("I did stuff");
+    	}
     }
 }
