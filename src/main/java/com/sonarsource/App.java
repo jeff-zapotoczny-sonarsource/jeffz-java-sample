b4065afb5386b8e03cfbced75a3e9d4831762543
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2019-04-03 15:31:48 -0500

    Adding more bad code

--- src/main/java/com/sonarsource/App.java
+++ src/main/java/com/sonarsource/App.java
@@ -4,18 +4,25 @@ package com.sonarsource;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        App app = new App();
-        app.doStuff();
-    }
-    
-    public void doStuff() {
-    	for (int i = 0; i < 100; i++) {
-    		System.out.println("I did stuff");
-    	}
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		App app = new App();
+		app.doStuff();
+	}
+
+	public void doStuff() {
+		for (int i = 0; i < 100; i++) {
+			System.out.println("I did stuff");
+		}
+	}
+
+	public void doMoreStuff() {
+		for (int i = 0; i < 5;) {
+			System.out.println("do-nothing loop");
+		}
+		while (true) {
+			System.out.println("Infinite loop");
+		}
+	}
 }
