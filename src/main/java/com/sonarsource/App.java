410adbefce07a92988afee6ec30f1534072ad09b
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2019-04-26 14:37:22 -0500

    Using new log4j logger.

--- src/main/java/com/sonarsource/App.java
+++ src/main/java/com/sonarsource/App.java
@@ -1,21 +1,26 @@
 package com.sonarsource;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	protected static Logger logger = LogManager.getLogger();
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	logger.info("Hello World!");
         App app = new App();
         app.doStuff();
     }
     
     public void doStuff() {
     	for (int i = 0; i < 100; i++) {
-    		System.out.println("I did stuff");
+    		logger.info("I did stuff");
     	}
     }
     
