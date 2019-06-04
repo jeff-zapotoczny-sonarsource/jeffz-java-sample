a5532524aabcbf3f17b5aae8dcc8fe6b02eff633
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2019-06-04 16:05:12 -0500

    Cleaning up code

--- src/main/java/com/sonarsource/App.java
+++ src/main/java/com/sonarsource/App.java
@@ -18,15 +18,11 @@ public class App
 	}
 
 	public int doStuff() {
-		try {
-			int sum = 0;
-			for (int i = 0; i < 5; i++) {
-				sum += i;
-			}
-			return sum;
-		} catch (Exception e) {
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			sum += i;
 		}
-		return -1;
+		return sum;
 	}
 
 }
